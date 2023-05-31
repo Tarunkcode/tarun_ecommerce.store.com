10d96d7c77930c6e41d023f61b87e7030fa7ed3c
Tarunkcode <tarunkumar43461@gmail.com>
2023-05-31 13:36:42 +0530

    first commit

---
+++ frontend/store.view/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import * as React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tarun ECommerce Store</h1>
+        <h3>Initail Project Set Up Completed on Date : 31-May-2023</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
